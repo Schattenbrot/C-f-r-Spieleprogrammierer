db9ffcd162c006fcfd61a09e80f2e6fe4f75283a
Schattenbrot <aaron.machill@web.de>
2018-07-20 01:03:09 +0200

    First commit.

---
+++ C++ für Spieleprogrammierer/Listing 1/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * C++ für Spieleprogrammierer
+ * Listing 1.1
+ * It will show a greetingstext.
+ * 
+ * Author:	Aaron
+ * Date:	20.7.2018
+ */
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Here comes the competition." << std::endl;
+
+	system("PAUSE");
+	return 0;
+}
